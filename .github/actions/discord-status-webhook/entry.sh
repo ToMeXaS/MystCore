6ecb97c89d164c343dafacb0cc9b67f35ff57b91
ToMeXaS <tomex495@gmail.com>
2025-07-07 19:18:47 +0300

    Fix JSON formatting in EXTRA_FIELDS to ensure consistent structure

--- .github/actions/discord-status-webhook/entry.sh
+++ .github/actions/discord-status-webhook/entry.sh
@@ -74,7 +74,7 @@ read -r -d '' PAYLOAD <<EOF
       { "name": "Repository", "value": "[$REPO]($REPO_URL)", "inline": true },
       { "name": "Branch", "value": "[$BRANCH]($BRANCH_URL)", "inline": true },
       { "name": "Commit", "value": "[$GIT_HASH]($COMMIT_URL)", "inline": true },
-      $( [[ -n "$EXTRA_FIELDS" ]] && echo "$EXTRA_FIELDS" )
+      $( [[ -n "$EXTRA_FIELDS" ]] && echo ", $EXTRA_FIELDS" )
       { "name": " ", "value": "[[View Run Action]]($RUN_URL)", "inline": false }
 
     ],
