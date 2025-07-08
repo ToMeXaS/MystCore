ebf08ce22ba63b72067cde46b4b4a64907620d0e
ToMeXaS <tomex495@gmail.com>
2025-07-08 19:44:25 +0300

    Refactor entry.sh to simplify changed files list processing and remove unnecessary loop

--- .github/actions/discord-commit-webhook/entry.sh
+++ .github/actions/discord-commit-webhook/entry.sh
@@ -15,20 +15,7 @@ current_length=0
 max_files=5
 MAX_LENGTH=900
 
-while IFS= read -r f; do
-  [ -z "$f" ] && continue
-  fname=$(basename "$f")
-  url="https://github.com/$REPO/blob/$GIT_HASH/$f"
-  line="- [$fname]($url)\n"
-  new_length=$((current_length + ${#line}))
-  if (( new_length > MAX_LENGTH )) || ((file_count >= max_files)); then
-    CHANGED_FILES_LIST="${CHANGED_FILES_LIST}...and more files not shown."
-    break
-  fi
-  CHANGED_FILES_LIST="${CHANGED_FILES_LIST}${line}"
-  current_length=$new_length
-  ((file_count++))
-done < <(git diff --name-only "$GIT_HASH"^ "$GIT_HASH")
+
 
 if [[ -z "$CHANGED_FILES_LIST" ]]; then
   CHANGED_FILES_LIST="No files changed."
